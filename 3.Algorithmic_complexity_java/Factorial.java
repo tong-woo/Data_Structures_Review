739e0b778cde6898af91dd81577c653dc1146671
Tong Wu <jimwu@kaist.ac.kr>
2022-03-16 11:34:36 +0100

    added recursion vs iterative

---
+++ 3.Algorithmic_complexity_java/Factorial.java
@@ -0,0 +1,34 @@
+
+
+public class Factorial {
+
+    public static int iterativeFactorial(int n) {
+        int result = 1;
+
+        while (n > 0) {
+            result *= n;
+            n -= 1;
+        }
+        return result;
+    }
+
+    public static int recursiveFactorial(int n) {
+        //This is the recursive step
+
+        if (n>0) {
+            System.out.println("Execution context: " + n);
+            return n*recursiveFactorial(n - 1);
+        }else{
+            return 1;
+        }
+
+    }
+
+    public static void main(String[] args){
+        int recursiveSolution = recursiveFactorial(10);
+        System.out.println("The recursive solution to 10! is: " + recursiveSolution);
+
+        int iterativeSolution = iterativeFactorial(10);
+        System.out.println("The iterative solution to 10! is: " + iterativeSolution);
+    }
+}
---
+++ 3.Algorithmic_complexity_java/Recursive.java
@@ -0,0 +1,25 @@
+/**
+ * base case and recursive step is important
+ * Problem Type: problems that require the execution of an action multiple times until
+ * a condition is met. For many problems, a recursive solution will result in fewer lines
+ * of code and will be easier to comprehend than a solution that uses a for or while loop.
+ *
+ *
+ * Recursion includes calling the function itself in the function
+ *
+ * Iteration is a repetition of a block of code
+ *
+ *
+ * start by thinking about potential recursive and base cases for your solution.
+ */
+
+
+
+public class Recursive {
+
+    public static void main(String[] args){
+
+
+
+    }
+}
